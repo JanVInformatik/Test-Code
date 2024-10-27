77a394c480e1ac76c8d6a89011e94b135ea5394c
FlyingPandaDev <janvogele2007@icloud.com>
2024-10-27 07:34:53 +0100

    .

---
+++ test2.cpp
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int num1, num2, result;
+
+  printf("Enter two integers: ");
+  scanf("%d %d", &num1, &num2);
+
+  result = num1 + num2;
+  return 0;
+}
