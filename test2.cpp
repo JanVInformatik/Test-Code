2ed2e20e16b49e909a7a4b6459d939f57cf4f366
FlyingPandaDev <janvogele2007@icloud.com>
2024-10-27 07:47:01 +0100

    .

--- test2.cpp
+++ test2.cpp
@@ -1,12 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <stdio.h>
+const int N = 0;
 
-int main(int argc, char *argv[]) {
-  int num1, num2, result;
-
-  printf("Enter two integers: ");
-  scanf("%d %d", &num1, &num2);
-
-  result = num1 + num2;
-  return 0;
-}
+int main() {}
