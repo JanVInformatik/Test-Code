c2b62f7113f330587e40bcacd1f584d0a44925f6
JanV <janvogele2007@icloud.com>
2024-11-03 18:47:31 +0100

    .

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+  public static void main(String[] args) {
+    int index = 0;
+
+
+  while(!element.equals
+  (sortedList.get(index))
+    && sortedList. size() > ++index) 
+    return index < sortedList.size)?index:-1;   
+  }
+}
