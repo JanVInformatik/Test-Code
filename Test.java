fb9ba242e42504f44f0679b76412d06168ef3a64
JanV <janvogele2007@icloud.com>
2024-11-03 18:50:07 +0100

    .

--- Test.java
+++ Test.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class Test {
   public static void main(String[] args) {
     int index = 0;
+    ArrayList sortedList = new ArrayList<String>();
+    String element = "test";
 
-
-  while(!element.equals
-  (sortedList.get(index))
-    && sortedList. size() > ++index) 
-    return index < sortedList.size)?index:-1;   
+    while (!element.equals(sortedList.get(index))
+        && sortedList.size() > ++index)
+      return index < sortedList.size ? index : -1;
   }
 }
