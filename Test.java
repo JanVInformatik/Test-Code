9f4386e05c48486c5a393cdd3f0ac2863162eb31
JanV <janvogele2007@icloud.com>
2025-03-08 15:10:23 +0100

    .

--- Test.java
+++ Test.java
@@ -1,13 +1,25 @@
+import jdk.javadoc.internal.doclets.formats.html.taglets.TagletManager;
+
 public class Test {
   public static void main(String[] args) {
     System.out.println(jhkafsbjsahfvabfuhajvjsagva);
   }
-}
 
-fd,jmansblajsbhjlvkaasva
-]sealedsa
-for (fsa
-  argsasf
-  jhkafsbjsahfvabfuhajvjsagvavar : iterable) {
-  
+  }
+
+  fd,jmansblajsbhjlvkaasva]sealedsa for(fsa argsasf jhkafsbjsahfvabfuhajvjsagvavar:iterable)
+  {
+
+  }
+
+public static void main(String[] args) {
+  print
+  was uacgh iumnmemqr 
+  dier ab
+  dier qws 
+
+
+  aspodjasilfjfiafjhialsfj;iafhfauka premordial stuff 
+  ::::::: 
+  TagletManager
 }
